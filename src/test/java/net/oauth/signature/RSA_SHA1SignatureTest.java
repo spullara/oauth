6b8e9227bb96c33a531f4bb566d7c2c0c0c33734
dirk.balfanz <dirk.balfanz@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2007-12-03 06:34:18 +0000

    Added the ability to specify PKCS#8-encoded private keys and X509-encoded public keys and certificates for use in RSA-SHA1 signing/verifying consumers. See comments in RSA_SHA1 class.

--- src/test/java/net/oauth/signature/RSA_SHA1SignatureTest.java
+++ src/test/java/net/oauth/signature/RSA_SHA1SignatureTest.java
@@ -16,11 +16,14 @@
 
 package net.oauth.signature;
 
+import java.io.ByteArrayInputStream;
 import java.math.BigInteger;
 import java.net.URL;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.security.spec.EncodedKeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
@@ -61,10 +64,28 @@ public class RSA_SHA1SignatureTest extends TestCase {
           + "mzyvIgP08N4S0IFzEURkV1wp/IpH7kH41EtbmUmrXSwfNZsnQRE5SYSOhh+LcK2w"
           + "yQkdgcMv11l4KoBkcwIDAQAB";
 
-    protected OAuthConsumer client;
-    protected OAuthConsumer server;
-    protected PublicKey publicKey;
-    protected PrivateKey privateKey;
+    private static final String CERTIFICATE =
+            "-----BEGIN CERTIFICATE-----\n"
+          + "MIIBpjCCAQ+gAwIBAgIBATANBgkqhkiG9w0BAQUFADAZMRcwFQYDVQQDDA5UZXN0\n"
+          + "IFByaW5jaXBhbDAeFw03MDAxMDEwODAwMDBaFw0zODEyMzEwODAwMDBaMBkxFzAV\n"
+          + "BgNVBAMMDlRlc3QgUHJpbmNpcGFsMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKB\n"
+          + "gQC0YjCwIfYoprq/FQO6lb3asXrxLlJFuCvtinTF5p0GxvQGu5O3gYytUvtC2JlY\n"
+          + "zypSRjVxwxrsuRcP3e641SdASwfrmzyvIgP08N4S0IFzEURkV1wp/IpH7kH41Etb\n"
+          + "mUmrXSwfNZsnQRE5SYSOhh+LcK2wyQkdgcMv11l4KoBkcwIDAQABMA0GCSqGSIb3\n"
+          + "DQEBBQUAA4GBAGZLPEuJ5SiJ2ryq+CmEGOXfvlTtEL2nuGtr9PewxkgnOjZpUy+d\n"
+          + "4TvuXJbNQc8f4AMWL/tO9w0Fk80rWKp9ea8/df4qMq5qlFWlx6yOLQxumNOmECKb\n"
+          + "WpkUQDIDJEoFUzKMVuJf4KO/FJ345+BNLGgbJ6WujreoM1X/gYfdnJ/J\n"
+          + "-----END CERTIFICATE-----";
+
+    protected OAuthConsumer clientJceKey;
+    protected OAuthConsumer clientDerKey;
+    protected OAuthConsumer clientPemKey;
+    protected OAuthConsumer serverJceKey;
+    protected OAuthConsumer serverDerKey;
+    protected OAuthConsumer serverPemKey;
+    protected OAuthConsumer serverX509JceCert;
+    protected OAuthConsumer serverX509DerCert;
+    protected OAuthConsumer serverX509PemCert;
 
     @Override
     protected void setUp() throws Exception {
@@ -72,13 +93,18 @@ public class RSA_SHA1SignatureTest extends TestCase {
                 new X509EncodedKeySpec(OAuthSignatureMethod
                         .decodeBase64(PUBLIC_KEY));
         KeyFactory fac = KeyFactory.getInstance("RSA");
-        publicKey = fac.generatePublic(pubKeySpec);
+        PublicKey publicKey = fac.generatePublic(pubKeySpec);
 
         EncodedKeySpec privKeySpec =
                 new PKCS8EncodedKeySpec(OAuthSignatureMethod
                         .decodeBase64(PRIVATE_KEY));
         fac = KeyFactory.getInstance("RSA");
-        privateKey = fac.generatePrivate(privKeySpec);
+        PrivateKey privateKey = fac.generatePrivate(privKeySpec);
+
+        CertificateFactory certFac = CertificateFactory.getInstance("X509");
+        ByteArrayInputStream in = new ByteArrayInputStream(
+                                                        CERTIFICATE.getBytes());
+        X509Certificate cert = (X509Certificate)certFac.generateCertificate(in);
 
         String url1 = "http://www.google.com/url1 ";
         String url2 = "http://www.google.com/url2";
@@ -86,24 +112,47 @@ public class RSA_SHA1SignatureTest extends TestCase {
         OAuthServiceProvider provider =
                 new OAuthServiceProvider(url1, url2, url3);
 
-        client = new OAuthConsumer(url1, "google", null, provider);
-        client.setProperty (RSA_SHA1.PRIVATE_KEY, privateKey);
+        clientJceKey = new OAuthConsumer(url1, "google", null, provider);
+        clientJceKey.setProperty (RSA_SHA1.PRIVATE_KEY, privateKey);
+        serverJceKey = new OAuthConsumer(url1, "google", null, provider);
+        serverJceKey.setProperty(RSA_SHA1.PUBLIC_KEY, publicKey);
 
-        server = new OAuthConsumer(url1, "google", null, provider);
-        server.setProperty(RSA_SHA1.PUBLIC_KEY, publicKey);
+        clientDerKey = new OAuthConsumer(url1, "google", null, provider);
+        clientDerKey.setProperty (RSA_SHA1.PRIVATE_KEY, deBase64(PRIVATE_KEY));
+        serverDerKey = new OAuthConsumer(url1, "google", null, provider);
+        serverDerKey.setProperty(RSA_SHA1.PUBLIC_KEY, deBase64(PUBLIC_KEY));
 
+        clientPemKey = new OAuthConsumer(url1, "google", null, provider);
+        clientPemKey.setProperty (RSA_SHA1.PRIVATE_KEY, PRIVATE_KEY);
+        serverPemKey = new OAuthConsumer(url1, "google", null, provider);
+        serverPemKey.setProperty(RSA_SHA1.PUBLIC_KEY, PUBLIC_KEY);
+
+        serverX509JceCert = new OAuthConsumer(url1, "google", null, provider);
+        serverX509JceCert.setProperty(RSA_SHA1.X509_CERTIFICATE, cert);
+
+        serverX509DerCert = new OAuthConsumer(url1, "google", null, provider);
+        serverX509DerCert.setProperty(RSA_SHA1.X509_CERTIFICATE,
+                                      cert.getEncoded());
+
+        serverX509PemCert = new OAuthConsumer(url1, "google", null, provider);
+        serverX509PemCert.setProperty(RSA_SHA1.X509_CERTIFICATE, CERTIFICATE);
+
+    }
+
+    private byte[] deBase64(String string) {
+        return OAuthSignatureMethod.decodeBase64(string);
     }
 
     // we don't need unpredictability, just uniqueness
     private static final Random random = new Random();
 
-    public static OAuthMessage prepareRequestMessage(OAuthConsumer consumer,
+    private static OAuthMessage prepareRequestMessage(OAuthConsumer consumer,
                                                      String httpMethod,
                                                      URL url,
                                                      String signatureMethod) {
         OAuthMessage message = new OAuthMessage(httpMethod,
-                                                url.toString(),
-                                                new ArrayList<OAuth.Parameter>());
+                                              url.toString(),
+                                              new ArrayList<OAuth.Parameter>());
         message.addParameter(OAuth.OAUTH_SIGNATURE_METHOD, signatureMethod);
         message.addParameter(OAuth.OAUTH_VERSION, OAuth.VERSION_1_0);
         message.addParameter(OAuth.OAUTH_CONSUMER_KEY, consumer.consumerKey );
@@ -118,51 +167,126 @@ public class RSA_SHA1SignatureTest extends TestCase {
         return message;
     }
 
-    public void testSignVerify() throws Exception {
-        String urlStr = "http://www.google.com/foo?param1=value1&param2=value2 ";
-        URL url = new URL(urlStr);
+    private static void doTests(OAuthMessage message,
+                                OAuthAccessor clientAccessor,
+                                OAuthAccessor serverAccessor) throws Exception {
 
-        OAuthMessage message = prepareRequestMessage(client,
-                                                     "GET",
-                                                     url,
-                                                     OAuth.RSA_SHA1);
-
-        System.out.println("url before: " + url.toString());
-        OAuthAccessor accessor1 = new OAuthAccessor(client);
-        message.sign(accessor1);
-
-        String form = OAuth.formEncode(message.getParameters());
-        System.out.println("url after: " + url.toString() + "&" + form);
-
-        OAuthAccessor accessor2 = new OAuthAccessor(server);
+        message.sign(clientAccessor);
 
         try {
-            message.validateSignature(accessor2);
+            message.validateSignature(serverAccessor);
         } catch(Exception e) {
             fail("message should have verified, but didn't");
         }
 
         try {
-            message.sign(accessor2);
-            fail("shouldn't be able to sign message without private key, but did");
+            message.sign(serverAccessor);
+            fail("shouldn't be able to sign message without private key, " +
+                 "but did");
         } catch(IllegalStateException e) {
             // expected
         }
 
         try {
-            message.validateSignature(accessor1);
-            fail("shouldn't be able to verify message without public key, but did");
+            message.validateSignature(clientAccessor);
+            fail("shouldn't be able to verify message without public key, " +
+            	 "but did");
         } catch(IllegalStateException e) {
             // expected
         }
 
         message.addParameter("foo", "bar");
         try {
-            message.validateSignature (accessor2);
-            fail("modified message signature should not have validated, but did");
+            message.validateSignature (serverAccessor);
+            fail("modified message signature should not have validated, " +
+            	 "but did");
         } catch(OAuthProblemException e) {
             // to be expected
         }
 
+    }
+
+    public void testSignVerifyJce() throws Exception {
+        String urlStr = "http://www.google.com/foo?param1=value1&param2=value2";
+        URL url = new URL(urlStr);
+
+        OAuthMessage message = prepareRequestMessage(clientJceKey,
+                                                     "GET",
+                                                     url,
+                                                     OAuth.RSA_SHA1);
+
+        OAuthAccessor accessor1 = new OAuthAccessor(clientJceKey);
+        OAuthAccessor accessor2 = new OAuthAccessor(serverJceKey);
+        doTests(message, accessor1, accessor2);
+    }
+
+    public void testSignVerifyDer() throws Exception {
+        String urlStr = "http://www.google.com/foo?param1=value1&param2=value2";
+        URL url = new URL(urlStr);
+
+        OAuthMessage message = prepareRequestMessage(clientDerKey,
+                                                     "GET",
+                                                     url,
+                                                     OAuth.RSA_SHA1);
+
+        OAuthAccessor accessor1 = new OAuthAccessor(clientDerKey);
+        OAuthAccessor accessor2 = new OAuthAccessor(serverDerKey);
+        doTests(message, accessor1, accessor2);
+    }
+
+    public void testSignVerifyPem() throws Exception {
+        String urlStr = "http://www.google.com/foo?param1=value1&param2=value2";
+        URL url = new URL(urlStr);
+
+        OAuthMessage message = prepareRequestMessage(clientPemKey,
+                                                     "GET",
+                                                     url,
+                                                     OAuth.RSA_SHA1);
+
+        OAuthAccessor accessor1 = new OAuthAccessor(clientPemKey);
+        OAuthAccessor accessor2 = new OAuthAccessor(serverPemKey);
+        doTests(message, accessor1, accessor2);
+    }
+
+    public void testSignVerifyX509Jce() throws Exception {
+        String urlStr = "http://www.google.com/foo?param1=value1&param2=value2";
+        URL url = new URL(urlStr);
+
+        OAuthMessage message = prepareRequestMessage(clientJceKey,
+                                                     "GET",
+                                                     url,
+                                                     OAuth.RSA_SHA1);
+
+        OAuthAccessor accessor1 = new OAuthAccessor(clientJceKey);
+        OAuthAccessor accessor2 = new OAuthAccessor(serverX509JceCert);
+        doTests(message, accessor1, accessor2);
+    }
+
+    public void testSignVerifyX509Der() throws Exception {
+        String urlStr = "http://www.google.com/foo?param1=value1&param2=value2";
+        URL url = new URL(urlStr);
+
+        OAuthMessage message = prepareRequestMessage(clientJceKey,
+                                                     "GET",
+                                                     url,
+                                                     OAuth.RSA_SHA1);
+
+        OAuthAccessor accessor1 = new OAuthAccessor(clientJceKey);
+        OAuthAccessor accessor2 = new OAuthAccessor(serverX509DerCert);
+        doTests(message, accessor1, accessor2);
+    }
+
+    public void testSignVerifyX509Pem() throws Exception {
+        String urlStr = "http://www.google.com/foo?param1=value1&param2=value2";
+        URL url = new URL(urlStr);
+
+        OAuthMessage message = prepareRequestMessage(clientJceKey,
+                                                     "GET",
+                                                     url,
+                                                     OAuth.RSA_SHA1);
+
+        OAuthAccessor accessor1 = new OAuthAccessor(clientJceKey);
+        OAuthAccessor accessor2 = new OAuthAccessor(serverX509PemCert);
+        doTests(message, accessor1, accessor2);
     }
 }
