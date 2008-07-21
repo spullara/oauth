f65e2dd787a40ea9f604f097eb8c6f6068f5889d
jkristian@netflix.com <jkristian@netflix.com@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2008-07-21 20:13:01 +0000

    corrected copyright date

--- src/test/java/net/oauth/OAuthClientTest.java
+++ src/test/java/net/oauth/OAuthClientTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 Netflix, Inc.
+ * Copyright 2008 Netflix, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
