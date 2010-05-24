4acc7a1f35291957f0e71bb2245bdf13004db409
jmkristian@gmail.com <jmkristian@gmail.com@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2010-05-24 02:25:07 +0000

    added hashCode and equals

--- commons/src/main/java/net/oauth/OAuthServiceProvider.java
+++ commons/src/main/java/net/oauth/OAuthServiceProvider.java
@@ -38,4 +38,44 @@ public class OAuthServiceProvider implements Serializable {
         this.accessTokenURL = accessTokenURL;
     }
 
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result
+		+ ((accessTokenURL == null) ? 0 : accessTokenURL.hashCode());
+	result = prime * result
+		+ ((requestTokenURL == null) ? 0 : requestTokenURL.hashCode());
+	result = prime * result
+		+ ((userAuthorizationURL == null) ? 0 : userAuthorizationURL.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	OAuthServiceProvider other = (OAuthServiceProvider) obj;
+	if (accessTokenURL == null) {
+	    if (other.accessTokenURL != null)
+		return false;
+	} else if (!accessTokenURL.equals(other.accessTokenURL))
+	    return false;
+	if (requestTokenURL == null) {
+	    if (other.requestTokenURL != null)
+		return false;
+	} else if (!requestTokenURL.equals(other.requestTokenURL))
+	    return false;
+	if (userAuthorizationURL == null) {
+	    if (other.userAuthorizationURL != null)
+		return false;
+	} else if (!userAuthorizationURL.equals(other.userAuthorizationURL))
+	    return false;
+	return true;
+    }
+
 }
