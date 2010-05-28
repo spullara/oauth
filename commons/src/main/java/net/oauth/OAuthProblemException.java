cafd28f620dbe1ae0783f4d381c0a8e2b8f892e5
jmkristian@gmail.com <jmkristian@gmail.com@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2010-05-28 15:20:54 +0000

    decode oauth_problem_advice, in OAuthProblemException.toString

--- commons/src/main/java/net/oauth/OAuthProblemException.java
+++ commons/src/main/java/net/oauth/OAuthProblemException.java
@@ -116,21 +116,23 @@ public class OAuthProblemException extends OAuthException {
         try {
             final String eol = System.getProperty("line.separator", "\n");
             final Map<String, Object> parameters = getParameters();
-            for (String key : new String[] { URL, SIGNATURE_BASE_STRING }) {
+            for (String key : new String[] { OAuth.Problems.OAUTH_PROBLEM_ADVICE, URL,
+                    SIGNATURE_BASE_STRING }) {
                 Object value = parameters.get(key);
                 if (value != null)
                     s.append(eol + key + ": " + value);
             }
             Object msg = parameters.get(HTTP_REQUEST);
             if ((msg != null))
-                s.append(eol + ">>>>>>>> " + HTTP_REQUEST + ":" + eol + msg + eol + "<<<<<<<<");
+                s.append(eol + ">>>>>>>> " + HTTP_REQUEST + ":" + eol + msg);
             msg = parameters.get(HTTP_RESPONSE);
             if (msg != null) {
-                s.append(" " + HTTP_RESPONSE + ":" + eol + msg);
+                s.append(eol + "<<<<<<<< " + HTTP_RESPONSE + ":" + eol + msg);
             } else {
                 for (Map.Entry<String, Object> parameter : parameters.entrySet()) {
                     String key = parameter.getKey();
-                    if (URL.equals(key) || SIGNATURE_BASE_STRING.equals(key)
+                    if (OAuth.Problems.OAUTH_PROBLEM_ADVICE.equals(key)
+                            || URL.equals(key) || SIGNATURE_BASE_STRING.equals(key)
                             || HTTP_REQUEST.equals(key) || HTTP_RESPONSE.equals(key))
                         continue;
                     s.append(eol + key + ": " + parameter.getValue());
